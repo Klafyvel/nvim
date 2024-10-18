795cf78926e5f94d603808d015a80f1b475b5e07
Hugo Levy-Falk <hugo@klafyvel.me>
2024-10-18 15:44:43 +0200

    Upgrade smuggler.

--- lua/plugins/nvim-smuggler.lua
+++ lua/plugins/nvim-smuggler.lua
@@ -1,1 +1,8 @@
-require("smuggler").setup()
+require("smuggler").setup({
+   ui = {
+       qf_auto_refresh = true,
+       qf_auto_open = true,
+       qf_custom_text = true,
+       qf_custom_display = true,
+   } 
+})
