653918c54a0aa1680716432fbb350fe6e44ceaae
Hugo Levy-Falk <hugo@klafyvel.me>
2024-10-18 15:45:12 +0200

    Add grep picker to telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set("n", "<leader>ts", builtins.spell_suggest, { noremap = true, sile
 vim.keymap.set("n", "<leader>tk", builtins.keymaps, { noremap = true, silent = true, desc="Show keymaps" })
 vim.keymap.set("n", "<leader>tt", builtins.treesitter, { noremap = true, silent = true, desc="Telescope treesitter" })
 vim.keymap.set("n", "<leader>tb", builtins.buffers, { noremap = true, silent = true, desc="Telescope buffers" })
+vim.keymap.set("n", "<leader>tg", builtins.live_grep, { noremap = true, silent = true, desc="Telescope buffers" })
 vim.keymap.set("n", "<leader><leader>", builtins.builtin, { noremap = true, silent = true, desc="Telescope builtins" })
 
 require("telescope").setup {
